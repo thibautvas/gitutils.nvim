a1e34755194aa2e504505dfdcdd32e2792adc6b3
thibautvas <thibaut.vas@gmail.com>
2026-03-13 19:18:29 +0100

    refactor: modularize init.lua into actions

--- lua/gitutils/helpers.lua
+++ lua/gitutils/helpers.lua
@@ -9,19 +9,6 @@ M.rel_head = function(arg)
   return arg
 end
 
-M.prompt_action = function(subcmd, prompt, action, on_done)
-  vim.ui.input({ prompt = prompt }, function(arg)
-    if not arg or arg == "" then return end
-    arg = M.rel_head(arg)
-    action(arg)
-    if vim.v.shell_error ~= 0 then
-      vim.notify(string.format("Gitutils %s failed", subcmd), vim.log.levels.ERROR)
-      return
-    end
-    if on_done then on_done() end
-  end)
-end
-
 M.refresh_head = function()
   local ref = vim.fn.system("git rev-parse --abbrev-ref HEAD")
   if vim.v.shell_error ~= 0 then
@@ -31,46 +18,6 @@ M.refresh_head = function()
   local ref_clean = vim.trim(ref)
   local title = M.log("HEAD", 1, "%s")
   vim.g.gitutils_head = title .. " -> " .. ref_clean
-end
-
-M.rebase_exit = function(rebase_state)
-  local function on_exit(_, code)
-    if code ~= 0 then
-      vim.notify("Rebase interrupted with code " .. code, vim.log.levels.ERROR)
-      return
-    end
-    vim.schedule(function()
-      vim.cmd("checktime")
-      M.refresh_head()
-      -- TODO: check from subdir
-      local in_progress = vim.fn.isdirectory(".git/rebase-merge") == 1
-      if in_progress then
-        vim.notify("Rebase in progress")
-        return
-      end
-      vim.fn.delete(rebase_state.script)
-      vim.fn.delete(rebase_state.fifo)
-      vim.api.nvim_del_autocmd(rebase_state.au_id)
-      rebase_state = nil
-      vim.notify("Rebase done")
-    end)
-  end
-  return on_exit
-end
-
-M.diff_view = function(hash)
-  local filepath = vim.fn.expand("%")
-  local filetype = vim.bo.filetype
-  local content = vim.fn.systemlist({ "git", "show", hash .. ":" .. filepath })
-  vim.cmd("diffthis")
-  vim.cmd("vsplit")
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_win_set_buf(0, buf)
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, content)
-  vim.bo[buf].filetype = filetype
-  vim.bo[buf].bufhidden = "wipe"
-  vim.cmd("diffthis")
-  vim.cmd("wincmd p")
 end
 
 return M
