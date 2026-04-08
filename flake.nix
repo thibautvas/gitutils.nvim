a6c93c7285146469be097d60e4c36e910287e700
thibautvas <thibaut.vas@gmail.com>
2026-04-09 00:04:15 +0200

    chore: upgrade to nvim 0.12 and partially rewrite flake

--- flake.nix
+++ flake.nix
@@ -21,62 +21,98 @@
             src = ./.;
           };
 
-          customRC = ''
+          luaRcContent = ''
+            vim.g.mapleader = " "
+            vim.o.wrap = false
+            vim.o.number = true
+            vim.o.cursorline = true
+            vim.o.signcolumn = "yes"
+          '';
+
+          fromLua = config: ''
             lua << EOF
-              vim.opt.number = true
-              vim.opt.signcolumn = "yes"
-
-              local gu = require("gitutils")
-              gu.setup()
-
-              vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
-                pattern = "*",
-                callback = require("gitutils.helpers").refresh_head
-              })
-
-              vim.opt.rulerformat = '%66(%{g:gitutils_head}%= %l,%c%)'
-
-              vim.keymap.set("n", "<space>hc", gu.commit, { desc = "Gitutils commit" })
-              vim.keymap.set("n", "<space>he", gu.extend, { desc = "Gitutils extend" })
-              vim.keymap.set("n", "<space>hb", gu.checkout, { desc = "Gitutils checkout" })
-              vim.keymap.set("n", "<space>hx", gu.rebase, { desc = "Gitutils interactive rebase" })
-              vim.keymap.set("n", "<space>hv", gu.continue, { desc = "Gitutils rebase continue" })
-
-              vim.keymap.set("n", "<space>hf", function()
-                require("gitsigns").stage_hunk(nil, {}, function()
-                  gu.extend()
-                end)
-              end, { desc = "Gitsigns stage and Gitutils extend" })
-              vim.keymap.set("v", "<space>hf", function()
-                require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") }, {}, function()
-                  gu.extend()
-                end)
-              end, { desc = "Gitsigns stage and Gitutils extend" })
-
-              vim.keymap.set("n", "<space>ht", gu.diffthis, { desc = "Gitutils diff buffer" })
-              vim.keymap.set("n", "<space>hg", gu.diff, { desc = "Gitutils diff repo" })
-              vim.keymap.set("n", "]g", function()
-                gu.qf_diff("next")
-              end, { desc = "Gitutils next diff" })
-              vim.keymap.set("n", "[g", function()
-                gu.qf_diff("prev")
-              end, { desc = "Gitutils prev diff" })
+              ${config}
             EOF
           '';
 
-          wrappedNvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
-            pkgs.neovimUtils.makeNeovimConfig {
-              inherit customRC;
-              plugins = with pkgs.vimPlugins; [
-                gitutils-nvim
-                gitsigns-nvim
-              ];
+          plugins = with pkgs.vimPlugins; [
+            {
+              plugin = gitsigns-nvim;
+              config = fromLua ''
+                local gs = require("gitsigns")
+
+                for dir, keymap in pairs({
+                  next = "<M-h>",
+                  prev = "<M-H>",
+                }) do
+                  vim.keymap.set({ "n", "v" }, keymap, function()
+                    gs.nav_hunk(dir, { target = "all" }, function()
+                      vim.cmd("norm! zz")
+                    end)
+                  end, { desc = "Gitsigns " .. dir .. " hunk" })
+                end
+
+                for action, keymap in pairs({
+                  stage = "<leader>ha",
+                  reset = "<leader>hr",
+                }) do
+                  vim.keymap.set("n", keymap, gs[action .. "_hunk"], {
+                    desc = "Gitsigns " .. action .. " hunk",
+                  })
+                  vim.keymap.set("v", keymap, function()
+                    gs[action .. "_hunk"]({ vim.fn.line("."), vim.fn.line("v") })
+                  end, { desc = "Gitsigns " .. action .. " hunk (visual)" })
+                end
+
+                vim.keymap.set("n", "<leader>hd", gs.preview_hunk_inline, { desc = "Gitsigns diff hunk" })
+
+                vim.keymap.set({ "o", "x" }, "ih", gs.select_hunk, { desc = "Gitsigns select hunk" })
+              '';
             }
-          );
+            {
+              plugin = gitutils-nvim;
+              config = fromLua ''
+                local gu = require("gitutils")
+                gu.setup()
+
+                vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged" }, {
+                  pattern = "*",
+                  callback = require("gitutils.helpers").refresh_head,
+                })
+
+                vim.opt.rulerformat = "%50(%{g:gitutils_head}%= %l,%c%)"
+
+                vim.keymap.set("n", "<leader>hc", gu.commit, { desc = "Gitutils commit" })
+                vim.keymap.set("n", "<leader>he", gu.extend, { desc = "Gitutils extend" })
+                vim.keymap.set("n", "<leader>hb", gu.checkout, { desc = "Gitutils checkout" })
+                vim.keymap.set("n", "<leader>hx", gu.rebase, { desc = "Gitutils interactive rebase" })
+                vim.keymap.set("n", "<leader>hv", gu.continue, { desc = "Gitutils rebase continue" })
+
+                vim.keymap.set("n", "<leader>hf", function()
+                  require("gitsigns").stage_hunk(nil, {}, gu.extend)
+                end, { desc = "Gitsigns stage and Gitutils extend" })
+                vim.keymap.set("v", "<leader>hf", function()
+                  require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") }, {}, gu.extend)
+                end, { desc = "Gitsigns stage and Gitutils extend" })
+
+                vim.keymap.set("n", "<leader>ht", gu.diffthis, { desc = "Gitutils diff buffer" })
+                vim.keymap.set("n", "<leader>hg", gu.diff, { desc = "Gitutils diff repo" })
+                vim.keymap.set("n", "]g", function()
+                  gu.qf_diff("next")
+                end, { desc = "Gitutils next diff" })
+                vim.keymap.set("n", "[g", function()
+                  gu.qf_diff("prev")
+                end, { desc = "Gitutils prev diff" })
+              '';
+            }
+          ];
+
         in
         {
           default = gitutils-nvim;
-          nvim = wrappedNvim;
+          nvim = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
+            inherit luaRcContent plugins;
+          };
         }
       );
 
